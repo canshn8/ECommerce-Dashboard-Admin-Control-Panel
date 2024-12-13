02945ab43b9a4bf72f79febbcd02bc39c2a2e425
Can <99124310+canshn8@users.noreply.github.com>
2024-12-13 22:19:45 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyBvZ3FEmGMzA7Cvkz6sno4-A8cKDu0J8Lw",
-  authDomain: "commerce-1ca89.firebaseapp.com",
-  projectId: "commerce-1ca89",
-  storageBucket: "commerce-1ca89.appspot.com",
-  messagingSenderId: "603165024566",
-  appId: "1:603165024566:web:e0c5fbe3404d745cc25397",
-  measurementId: "G-CQPTQ7HE2B"
+  //Firebase Config 
+  //Private Content  
 };
 
 const app = initializeApp(firebaseConfig);
